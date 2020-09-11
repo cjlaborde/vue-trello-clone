c8833f26a739db65385a4abf5debc4761c04c7e8
Christian <cjlaborde@gmail.com>
2020-09-11 01:35:21 -0400

    Adding New Colums

--- src/views/Board.vue
+++ src/views/Board.vue
@@ -39,6 +39,16 @@
           />
         </div>
       </div>
+
+      <div class="column flex">
+        <input
+          type="text"
+          class="p-2 mr-2 flex-grow"
+          placeholder="New Column Name"
+          v-model="newColumnName"
+          @keyup.enter="createColumn"
+        />
+      </div>
     </div>
 
     <div class="task-bg" v-if="isTaskOpen" @click.self="close">
@@ -51,6 +61,11 @@
 import { mapState } from "vuex";
 
 export default {
+  data() {
+    return {
+      newColumnName: "",
+    };
+  },
   computed: {
     ...mapState(["board"]),
     isTaskOpen() {
@@ -70,6 +85,13 @@ export default {
         name: e.target.value,
       });
       e.target.value = "";
+    },
+    createColumn() {
+      this.$store.commit("CREATE_COLUMN", {
+        name: this.newColumnName,
+      });
+
+      this.newColumnName = "";
     },
     pickupTask(e, taskIndex, fromColumnIndex) {
       e.dataTransfer.effectAllowed = "move";
